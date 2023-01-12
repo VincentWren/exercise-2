2c7259fc17a6c04118e7d90859dfcb8f9aa0df13
Vincent Wren <vincentwren@BCCSs-MacBook-Pro.local>
2023-01-12 11:13:17 -0600

    My work

---
+++ exercise#2.js
@@ -0,0 +1,9 @@
+// The variable that is used to keep a value the same is called 'const'
+let num = 20; 
+num = -- 5;
+num = num + 7; 
+num -- ;
+num = "This is not a number"; 
+var blank;
+console.log("blank");
+let blank;
